4617d14b009a70b4e12377f10595f01330e2ba92
Vijay J <81014244+yoyozaemon@users.noreply.github.com>
2022-12-24 09:10:57 +0530

    LSWRC solution added

---
+++ LongestSubstringWithoutRepeatingCharacters/LSWRC.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        def check(start, end):
+            chars = set()
+            for i in range(start, end + 1):
+                c = s[i]
+                if c in chars:
+                    return False
+                chars.add(c)
+            return True
+
+        n = len(s)
+
+        res = 0
+        for i in range(n):
+            for j in range(i, n):
+                if check(i, j):
+                    res = max(res, j - i + 1)
+        return res
