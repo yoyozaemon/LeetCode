0ae248bd85e6916a109136860c5b16fce3cdafd9
Vijay J <81014244+yoyozaemon@users.noreply.github.com>
2022-12-27 10:45:50 +0530

    String to integer added

---
+++ c/StringtoInteger/sti.c
@@ -0,0 +1,17 @@
+int myAtoi(char * s){
+ int sign = 1, base = 0, i = 0;
+    while (s[i] == ' ') 
+    { i++; }
+    if (s[i] == '-' || s[i] == '+') 
+    {
+        sign = 1 - 2 * (s[i++] == '-'); 
+    }
+    while (s[i] >= '0' && s[i] <= '9') 
+    {
+        if (base >  INT_MAX / 10 || (base == INT_MAX / 10 && s[i] - '0' > 7)) 
+        {
+            if (sign == 1) return INT_MAX;
+            else return INT_MIN;
+        }
+        base  = 10 * base + (s[i++] - '0');
+    }
