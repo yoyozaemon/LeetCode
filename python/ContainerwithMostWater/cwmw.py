7f2b56755cfeec0c84a364f03d854dcb13eff932
Vijay J <81014244+yoyozaemon@users.noreply.github.com>
2023-01-07 08:04:31 +0530

    Container with most water solution added

---
+++ python/ContainerwithMostWater/cwmw.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left, right = 0, len(height) - 1
+        max_area = 0
+        while left < right:
+            max_area = max(max_area,min(height[left],height[right])* (right - left))
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return max_area
